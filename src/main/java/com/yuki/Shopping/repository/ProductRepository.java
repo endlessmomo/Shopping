e1b7fa25d92ef28a38924cf1b9ed5fdb28858678
endlessmomo <soohyuk96@naver.com>
2022-11-24 00:29:24 +0900

    feat(repository, ProductRepository) : ProductRepository 생성

---
+++ src/main/java/com/yuki/Shopping/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.yuki.Shopping.repository;
+
+import com.yuki.Shopping.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
