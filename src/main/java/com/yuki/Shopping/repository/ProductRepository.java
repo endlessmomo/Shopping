03a85819ff0d05dce8c24e2046df405b4fdd8de2
endlessmomo <soohyuk96@naver.com>
2022-11-24 16:12:00 +0900

    feat(repository, ProductRepository) : ProductRepository 생성 및 메소드 구현 - productName을 이용한 조회 - productName과 productDetail을 이용한 조회 - 일정 price보다 낮은 상품을 찾는 조회 - 일정 price보다 낮은 상품을 내림차순으로 정렬하여 조회 - Jquery를 이용한 조회 - Querydsl을 이용한 조회

--- src/main/java/com/yuki/Shopping/repository/ProductRepository.java
+++ src/main/java/com/yuki/Shopping/repository/ProductRepository.java
@@ -2,7 +2,25 @@ package com.yuki.Shopping.repository;
 
 import com.yuki.Shopping.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository <Product,Long>, QuerydslPredicateExecutor <Product> {
+    List <Product> findByProductName(String productName);
+
+    List <Product> findByProductNameOrProductDetail(String productName, String productDetail);
+
+    List <Product> findByPriceLessThan(Integer price);
+
+    List <Product> findByPriceLessThanOrderByPriceAsc(Integer price);
+
+    @Query("select p from Product p where p.productDetail like %:productDetail% order by p.price desc")
+    List <Product> findByProductDetail(@Param("productDetail") String productDetail);
+
+    @Query(value = "select * from product p where p.product_detail like %:productDetail% order by p.price desc", nativeQuery = true)
+    List <Product> findByProductDetailByNative(@Param("productDetail") String productDetail);
 
 }
