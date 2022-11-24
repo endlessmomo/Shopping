9c77f6f61be7c80d4f29fdb5cd2a8ab7a6c12719
endlessmomo <soohyuk96@naver.com>
2022-11-24 17:15:17 +0900

    feat(dto, ProductDto) : ProductDto 생성

---
+++ src/main/java/com/yuki/Shopping/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package com.yuki.Shopping.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class ProductDto {
+    private Long id;
+    private String productName;
+    private Integer price;
+    private String productDetail;
+    private String sellStatus;
+    private LocalDateTime registered_date;
+    private LocalDateTime updated_date;
+}
