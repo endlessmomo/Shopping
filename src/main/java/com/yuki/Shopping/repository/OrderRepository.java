ebd25eb8dce2101557574c447c080cf97bf7cb84
endlessmomo <soohyuk96@naver.com>
2022-11-28 09:31:16 +0900

    feat(repository, OrderRepository) : OrderRepository 생성

---
+++ src/main/java/com/yuki/Shopping/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.yuki.Shopping.repository;
+
+import com.yuki.Shopping.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
