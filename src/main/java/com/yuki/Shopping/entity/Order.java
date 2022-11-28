6e3ce950cc77cb27c9bedb4667855e1891de8007
endlessmomo <soohyuk96@naver.com>
2022-11-28 09:25:23 +0900

    feat(entity, Order) : Order Entity 생성 - 한 명의 회원은 여러 번 주문을 할 수 있으므로 다대일 매핑

---
+++ src/main/java/com/yuki/Shopping/entity/Order.java
@@ -0,0 +1,38 @@
+package com.yuki.Shopping.entity;
+
+import com.yuki.Shopping.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Table(name="orders")
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order")
+    private List <OrderItem> orderItems = new ArrayList <>();
+
+    private LocalDateTime registered_date;
+    private LocalDateTime updated_date;
+
+
+}
