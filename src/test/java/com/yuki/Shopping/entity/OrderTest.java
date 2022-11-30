c9319b4b4f349e7b6b8ceb601ad13503bedde5bf
endlessmomo <soohyuk96@naver.com>
2022-11-30 23:57:49 +0900

    test(entity, OrderTest) : 고아객체 제거 테스트 확인

--- src/test/java/com/yuki/Shopping/entity/OrderTest.java
+++ src/test/java/com/yuki/Shopping/entity/OrderTest.java
@@ -1,6 +1,7 @@
 package com.yuki.Shopping.entity;
 
 import com.yuki.Shopping.constant.ProductStatus;
+import com.yuki.Shopping.repository.MemberRepository;
 import com.yuki.Shopping.repository.OrderRepository;
 import com.yuki.Shopping.repository.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -19,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
-@TestPropertySource(locations="classpath:application-test.properties")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class OrderTest {
     @Autowired
     OrderRepository orderRepository;
@@ -27,12 +28,14 @@ class OrderTest {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    MemberRepository memberRepository;
+
     @PersistenceContext
     EntityManager em;
 
 
-
-    public Product createItem(){
+    public Product createItem() {
         Product product = Product.builder()
                 .productName("테스트 상품")
                 .price(10000)
@@ -54,7 +57,7 @@ class OrderTest {
         Order order = new Order();
 
         //when
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             Product product = this.createItem();
             productRepository.save(product);
             OrderItem orderItem = OrderItem.builder()
@@ -73,5 +76,41 @@ class OrderTest {
         Order savedOrder = orderRepository.findById(order.getId())
                 .orElseThrow(EntityNotFoundException::new);
         assertEquals(3, savedOrder.getOrderItems().size());
+    }
+
+    public Order createOrder() {
+        Order order = new Order();
+
+        for (int i = 0; i < 5; i++) {
+            Product product = createItem();
+            productRepository.save(product);
+
+            OrderItem orderItem = OrderItem.builder()
+                    .product(product)
+                    .count(10)
+                    .orderPrice(1000)
+                    .order(order)
+                    .build();
+
+            order.getOrderItems().add(orderItem);
+        }
+
+        Member member = new Member();
+        memberRepository.save(member);
+
+        order.setMember(member);
+        orderRepository.save(order);
+        return order;
+    }
+
+    @Test
+    @DisplayName("고아객체 제거 테스트")
+    void orphanRemovalTest() {
+        //given
+        Order order = this.createOrder();
+        //when
+        order.getOrderItems().remove(0);
+        //then
+        em.flush();
     }
 }
