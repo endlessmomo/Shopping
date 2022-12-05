b00860d6fbccc1d9a60623b15c94daebd8c26414
endlessmomo <soohyuk96@naver.com>
2022-12-05 16:21:57 +0900

    feat(entity, BaseTimeEntity) : 등록일과 수정일을 관리하는 BaseTimeEntity 구현

---
+++ src/main/java/com/yuki/Shopping/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.yuki.Shopping.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime registeredTime;
+
+    @LastModifiedDate
+    private LocalDateTime updatedTime;
+}
