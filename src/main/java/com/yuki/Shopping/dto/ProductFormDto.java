95eda457f5f896c376dd6f1f2e62392afcc84899
endlessmomo <soohyuk96@naver.com>
2022-12-05 22:43:21 +0900

    feat(dto, ProductFormDto) : 상품 데이터 정보를 전달하는 DTO 구현

---
+++ src/main/java/com/yuki/Shopping/dto/ProductFormDto.java
@@ -0,0 +1,45 @@
+package com.yuki.Shopping.dto;
+
+import com.yuki.Shopping.constant.ProductStatus;
+import com.yuki.Shopping.entity.Product;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class ProductFormDto {
+    private Long id;
+
+    @NotBlank(message = "상품명은 필수 입력 값입니다.")
+    private String productName;
+
+    @NotNull(message = "가격은 필수 입력 값입니다.")
+    private Integer price;
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String productDetail;
+
+    @NotNull(message = "재고는 필수 입력 값입니다.")
+    private String stockCnt;
+
+    private ProductStatus productStatus;
+
+    private List <ProductImgDto> productImgDtoList = new ArrayList <>();
+
+    private List <Long> productImgIds = new ArrayList <>();
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public Product createProduct() {
+        return modelMapper.map(this, Product.class);
+    }
+
+    public static ProductFormDto of(Product product) {
+        return modelMapper.map(product, ProductFormDto.class);
+    }
+}
