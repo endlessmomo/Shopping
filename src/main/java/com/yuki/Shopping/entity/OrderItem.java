f5de4134fc4a20c536f4159cf2bf0726021c4b8a
endlessmomo <soohyuk96@naver.com>
2022-12-05 13:29:54 +0900

    feat(entity, OrderItem) : Persist 속성 추가

--- src/main/java/com/yuki/Shopping/entity/OrderItem.java
+++ src/main/java/com/yuki/Shopping/entity/OrderItem.java
@@ -16,11 +16,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne ( fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
-    @ManyToOne
+    @ManyToOne ( fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
