7d6af9f18cd2a4f32ec8f2249d623872896e9e47
endlessmomo <soohyuk96@naver.com>
2022-12-07 14:59:27 +0900

    feat(service, ProductImgService) : Image 관련 서비스 구현  - 이미지 파일을 가져와 레포에 저장  - 이미지 업데이트

---
+++ src/main/java/com/yuki/Shopping/service/ProductImgService.java
@@ -0,0 +1,40 @@
+package com.yuki.Shopping.service;
+
+import com.yuki.Shopping.entity.ProductImg;
+import com.yuki.Shopping.repository.ProductImgRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProductImgService {
+
+    @Value("${productImgLocation}")
+    private String productImgLocation;
+
+    private final ProductImgRepository productImgRepository;
+
+    private final FileService fileService;
+
+    public void saveProductImg(ProductImg productImg, MultipartFile productImgFile)
+            throws Exception
+        {
+            String oriImgName = productImgFile.getOriginalFilename();
+            String imgName = "";
+            String imgUrl = "";
+
+            if(!StringUtils.isEmpty(oriImgName)){
+                imgName = fileService.uploadFiles(productImgLocation, oriImgName
+                        , productImgFile.getBytes());
+                imgUrl = "/images/product/" + imgName;
+            }
+
+            productImg.updateProductImg(oriImgName, imgName, imgUrl);
+            productImgRepository.save(productImg);
+    }
+}
