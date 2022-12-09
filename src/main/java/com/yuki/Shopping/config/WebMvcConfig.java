9e8a267413d65197a3d3384baf55e353beaf1335
endlessmomo <soohyuk96@naver.com>
2022-12-09 18:14:34 +0900

    feat(config, WebMvcConfig) : 업로드한 파일을 읽어올 경로를 설정하기 위한 WebMvcConfig
    -addResourceHandelrs 메소드를 통해 자신의 로컬 컴퓨터에 업로드한 파일을 찾을 위치를 설정

---
+++ src/main/java/com/yuki/Shopping/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.yuki.Shopping.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Value("${uploadPath}")
+    String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations(uploadPath);
+    }
+}
