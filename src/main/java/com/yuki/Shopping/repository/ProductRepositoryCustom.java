50401246651fb34e400856ab8b07b97582d1b683
endlessmomo <soohyuk96@naver.com>
2022-12-10 21:54:23 +0900

    feat(repository, ProductRepositoryCustom) : 동적 Querysdl을 사용하기 위한 ProductRepositoryCustom 생성

---
+++ src/main/java/com/yuki/Shopping/repository/ProductRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.yuki.Shopping.repository;
+
+import com.yuki.Shopping.dto.ProductSearchDto;
+import com.yuki.Shopping.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductRepositoryCustom {
+    Page <Product> getAdminProductPage(ProductSearchDto productSearchDto, Pageable pageable);
+}
