23295d4dc721570165bf58a979413cf654d6a978
endlessmomo <soohyuk96@naver.com>
2022-12-12 12:30:34 +0900

    fix(config, WebMvcConfig) : 빈 주입 x @Configuration 어노테이션 추가

--- src/main/java/com/yuki/Shopping/config/WebMvcConfig.java
+++ src/main/java/com/yuki/Shopping/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.yuki.Shopping.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${uploadPath}")
     String uploadPath;
