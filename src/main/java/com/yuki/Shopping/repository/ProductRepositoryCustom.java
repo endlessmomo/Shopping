9549dcf9ca078bbbb1f25dc8e8a0eb47f5bdbdc0
endlessmomo <soohyuk96@naver.com>
2022-12-12 12:49:26 +0900

    feat(repository, ProductRepositoryCustom) : 메인 페이지에 보여줄 상품 리스트를 가져오는 메소드 생성

--- src/main/java/com/yuki/Shopping/repository/ProductRepositoryCustom.java
+++ src/main/java/com/yuki/Shopping/repository/ProductRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.yuki.Shopping.repository;
 
+import com.yuki.Shopping.dto.MainProductDto;
 import com.yuki.Shopping.dto.ProductSearchDto;
 import com.yuki.Shopping.entity.Product;
 import org.springframework.data.domain.Page;
@@ -7,4 +8,6 @@ import org.springframework.data.domain.Pageable;
 
 public interface ProductRepositoryCustom {
     Page <Product> getAdminProductPage(ProductSearchDto productSearchDto, Pageable pageable);
+
+    Page <MainProductDto> getMainProductPage(ProductSearchDto productSearchDto, Pageable pageable);
 }
