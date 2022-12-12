d3926b3f2e8cb6b0719c7ed3edcc4ec1b13ac52c
endlessmomo <soohyuk96@naver.com>
2022-12-12 12:54:00 +0900

    feat(controller, MainController) : View로 값을 던져주기 위한 GetMapping 메소드 구현

--- src/main/java/com/yuki/Shopping/controller/MainController.java
+++ src/main/java/com/yuki/Shopping/controller/MainController.java
@@ -1,12 +1,32 @@
 package com.yuki.Shopping.controller;
 
+import com.yuki.Shopping.dto.MainProductDto;
+import com.yuki.Shopping.dto.ProductSearchDto;
+import com.yuki.Shopping.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
-    @GetMapping(value="/")
-    public String main(){
+    private final ProductService productService;
+    @GetMapping(value = "/")
+    public String main(ProductSearchDto productSearchDto,
+            Optional <Integer> page, Model model) {
+        Pageable pageable = PageRequest.of(page.orElse(0), 6);
+        Page < MainProductDto> products =
+                productService.getMainProductPage(productSearchDto, pageable);
+        model.addAttribute("products", products);
+        model.addAttribute("productSearchDto", productSearchDto);
+        model.addAttribute("maxPage", 5);
+
         return "main";
     }
 }
