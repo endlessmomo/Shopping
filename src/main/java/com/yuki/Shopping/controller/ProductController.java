8bf4d2c43cda5079184eb000f82756402748aaa5
endlessmomo <soohyuk96@naver.com>
2022-12-12 09:41:14 +0900

    feat(controller, ProductController) : 상품 관리 화면 이동 및 조회한 상품 데이터를 화면에 전달하는 로직 구현

--- src/main/java/com/yuki/Shopping/controller/ProductController.java
+++ src/main/java/com/yuki/Shopping/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.yuki.Shopping.controller;
 
 import com.yuki.Shopping.dto.ProductFormDto;
+import com.yuki.Shopping.dto.ProductSearchDto;
+import com.yuki.Shopping.entity.Product;
 import com.yuki.Shopping.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -87,5 +93,19 @@ public class ProductController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping(value={"/admin/products", "/admin/products/{page}"})
+    public String productManage(ProductSearchDto productSearchDto
+            , @PathVariable("page")Optional<Integer> page, Model model){
+        Pageable pageable = PageRequest.of(page.orElse(0), 3);
+
+        Page<Product> products =
+                productService.getAdminProductPage(productSearchDto, pageable);
+        model.addAttribute("products", products);
+        model.addAttribute("productSearchDto", productSearchDto);
+        model.addAttribute("maxPage", 5);
+
+        return "product/productMng";
     }
 }
