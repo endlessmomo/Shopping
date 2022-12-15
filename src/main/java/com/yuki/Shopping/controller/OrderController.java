791f33288929fa05603b3df109a7a5ef995120d5
endlessmomo <soohyuk96@naver.com>
2022-12-16 00:28:25 +0900

    feat(controller, OrderController) : 주문을 위한 컨트롤러 클래스 추가

---
+++ src/main/java/com/yuki/Shopping/controller/OrderController.java
@@ -0,0 +1,49 @@
+package com.yuki.Shopping.controller;
+
+import com.yuki.Shopping.dto.OrderDto;
+import com.yuki.Shopping.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping(value = "/order")
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto
+            , BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            List <FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity <String>(sb.toString(),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        String email = principal.getName();
+        Long orderId;
+
+        try {
+            orderId = orderService.order(orderDto, email);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+}
