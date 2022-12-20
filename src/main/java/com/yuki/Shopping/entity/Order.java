76bd4df2e6d95d6624e6cf9e6b348c41bdc5a48c
endlessmomo <soohyuk96@naver.com>
2022-12-20 22:20:46 +0900

    feat(Order) : 주문 상품 객체를 만드는 메소드와 주문과 관련된 로직 구현

--- src/main/java/com/yuki/Shopping/entity/Order.java
+++ src/main/java/com/yuki/Shopping/entity/Order.java
@@ -35,5 +35,27 @@ public class Order {
     private LocalDateTime registered_date;
     private LocalDateTime updated_date;
 
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
+    public static Order createOrder(Member member, List<OrderItem> orderItemList) {
+        Order order = new Order();
+        order.setMember(member);
+        for(OrderItem orderItem : orderItemList) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
